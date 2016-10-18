b6a4a913a2344935f2125271c2fbceb6974b0244
Seungjae Moon <sjaymoon15@gmail.com>
2016-10-18 14:57:43 -0400

    add databinding component

--- src/app/databinding/databinding.component.ts
+++ src/app/databinding/databinding.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-databinding',
+  selector: 'fa-databinding',
   templateUrl: './databinding.component.html',
   styleUrls: ['./databinding.component.css']
 })
-export class DatabindingComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class DatabindingComponent {
+	stringInterpolation = 'this is string interpolation';
+	numberInterpolation = 2;
+	onTest() {
+		return true;
+	}
+	doThisWithEvent(event:string){
+		console.log(event);
+	}
 }
